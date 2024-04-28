d6796bd8ff1f0dd284a741a3150d4d1b8f88e6f0
michalangelo_cali <michelangelocali@hotmail.com>
2024-04-28 15:15:29 -0300

    feat: header

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      // {
-      //   source: '/como-funciona',
-      //   destination: 'http://localhost:3000/como-funciona',
-      //   basePath: false,
-      // },
-    ]
-  },
-}
+const nextConfig = {}
 
 export default nextConfig
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 
 export const Header = () => {
   return (
-    <div className="bg-stone-900 text-white flex justify-between items-center px-8 h-14">
-      <Image src="./logo.svg" width={110} height={110} alt="logo" />
+    <div className="bg-stone-900 text-white flex justify-between items-center px-8 h-16">
+      <Image src="./logo.svg" width={140} height={100} alt="logo" />
       <div className="flex gap-8">
         <text className="font-sans text-sm font-extralight">
           O que é o Batalha Esportiva?
